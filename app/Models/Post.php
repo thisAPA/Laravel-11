a2cde32d19b0e8d4639e16d5031075c930a8309a
thisAPA <161906721+thisAPA@users.noreply.github.com>
2024-11-14 09:18:36 +0700

    Post Model
    Menambahkan data pada tabel post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,12 +1,10 @@
 <?php
 
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Arr;
 
-class Post extends Model  
+class Post extends Model
 {
     protected $fillable = ['title', 'author', 'slug', 'body'];
 }
