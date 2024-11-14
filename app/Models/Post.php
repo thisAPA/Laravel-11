88dd395924ef7e615dc2e8dc7a67e139b413ea51
thisAPA <161906721+thisAPA@users.noreply.github.com>
2024-11-14 11:59:10 +0700

    Model Factories

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,9 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    use HasFactory;
+
+    // Properti dan metode lainnya pada model Post
     protected $fillable = ['title', 'author', 'slug', 'body'];
 }
