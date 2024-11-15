6fc4704b9d40859f62bfec7675206e99f876f9cb
thisAPA <161906721+thisAPA@users.noreply.github.com>
2024-11-15 16:07:19 +0700

    Post Category

--- database/migrations/2024_11_15_052628_create_categories_table.php
+++ database/migrations/2024_11_15_052628_create_categories_table.php
@@ -11,17 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->foreignId('author_id')->constrained(
-                table:'users',
-                indexName: 'posts_author_id'
-            );
+            $table->string('name');
             $table->string('slug')->unique();
-            $table->text('body');
             $table->timestamps();
-
         });
     }
 
@@ -30,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('categories');
     }
 };
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Support\Arr;
@@ -24,10 +25,13 @@ Route :: get('/posts/{post:slug}', function(Post $post){
     return view ('post', ['title' => 'Single Post', 'post' =>$post]);
 });
 
-Route :: get('/authors/{user}', function(User $user){
-    return view ('posts', ['title' => 'Articles by ' .$user->name, 'posts' =>$user->posts]);
+Route :: get('/authors/{user:username}', function(User $user){
+    return view ('posts', ['title' => count($user->posts) . ' Articles by ' .$user->name, 'posts' =>$user->posts]);
 });
 
+Route :: get('/categories/{category:slug}', function(Category $category){
+    return view ('posts', ['title' => 'Articles in : ' .$category->name, 'posts' =>$category->posts]);
+});
 
 Route::get('/contact', function(){
     return view('contact', ['title' => 'Silahkan Hubungi Kami']);
